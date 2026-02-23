5349596df43d9268b55aa302e1a03441761b032e
Shmedi <eduard.tupy@hotmail.com>
2026-02-23 00:28:41 -0500

    feat: added config provider to root layout, updated screen names and styling, removed modal.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,35 @@
-import { DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { JsonConfigProvider } from '@providers/JsonConfigProvider'
+import { DarkTheme, ThemeProvider } from '@react-navigation/native'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import 'react-native-reanimated'
 
+// layout
 export default function RootLayout() {
 
+  // render
   return (
-    <ThemeProvider value={DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
+    <ThemeProvider value={DarkTheme}>
+      <JsonConfigProvider>
+        <StatusBar style="auto" />
+        <Stack
+          initialRouteName='index'
+          screenOptions={{ contentStyle: { backgroundColor: '#14281E' }}}
+        >
+          <Stack.Screen
+            name="index"
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="settings"
+            options={{
+              presentation: 'modal',
+              headerShown: false,
+              contentStyle: { backgroundColor: '#C7C7C7' }
+            }}
+          />
+        </Stack>
+      </JsonConfigProvider>
     </ThemeProvider>
   )
 }
