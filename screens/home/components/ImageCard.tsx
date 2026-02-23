e01538c55df777aea009fd2c1e0281baf139ec5c
Shmedi <eduard.tupy@hotmail.com>
2026-02-23 00:33:28 -0500

    feat: created image card component for carousel

---
+++ screens/home/components/ImageCard.tsx
@@ -0,0 +1,41 @@
+import { Image } from 'expo-image'
+
+// types
+type ImageOrientation = 'portrait' | 'landscape' | 'square' | (string & {})
+type ImageCardProps = {
+  uri: string
+  orientation?: ImageOrientation
+}
+
+// component
+export function ImageCard({ uri, orientation = 'landscape' }: ImageCardProps) {
+
+  // render
+  return (
+    <>
+      <Image
+        style={{
+          borderRadius: 8,
+          aspectRatio: ratioFor(orientation)
+        }}
+        source={{ uri }}
+        contentFit="cover"
+      />
+    </>
+  )
+}
+
+// utilities
+function ratioFor(orientation: ImageOrientation) {
+  switch (orientation) {
+    case 'landscape':
+      return 3/2
+    case 'portrait':
+      return 2/3
+    case 'square':
+      return 1
+    default:
+      console.warn(`Unknown orientation value of ${orientation} provided. Falling back to landscape.`)
+      return 3/2
+  }
+}
